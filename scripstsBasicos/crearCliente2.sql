96a2699a15c676c2804e5eeac1b6e9a32b6de7f3
Byron Gregorio Calderón López <129338728+Byroncl@users.noreply.github.com>
2024-05-03 18:58:18 -0500

    Update crearCliente2.sql

--- scripstsBasicos/crearCliente2.sql
+++ scripstsBasicos/crearCliente2.sql
@@ -1,7 +1,7 @@
 Create table clientes(
 codigoCliente int identity(1,1) not null primary key,
 nombre varchar(150),
-dirección varchar(300),
+direccion varchar(300),
 email varchar(40),
 fechanacimiento date,
 edad int,
@@ -10,11 +10,11 @@ constraint ck_edad check (edad<18)
 go
 
 
-insert into clientes(nombre, dirección, email, edad) values
+insert into clientes(nombre, direccion, email, edad) values
 ('byron calderon', 'santa martha', 'coso@gmail.com', 17)
 
 
-insert into clientes(nombre, dirección, email, edad) values
+insert into clientes(nombre, direccion, email, edad) values
 ('jose perez', 'santa martha', 'coso1@gmail.com', 21)
 
 select * from clientes;
